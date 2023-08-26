9b90263c91f3c675e44d5690ee25f7e5fc99ab92
1 <a1@1ui-MacBookPro.local>
2023-08-26 23:11:05 +0900

    서비스개발

--- src/main/java/hello/hellospring/service/memberService.java
+++ src/main/java/hello/hellospring/service/memberService.java
@@ -26,4 +26,12 @@ public class memberService {
                     throw new IllegalStateException("이미 존재하는 회원입니다.");
                 });
     }
+
+    /**
+     * 전체 회원 조회
+     */
+
+    public List<Member> findMember(){
+
+    }
 }
