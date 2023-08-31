cd680010a376ed41862119ddbe3259dc823ed8b3
1 <a1@1ui-MacBookPro.local>
2023-08-31 21:32:44 +0900

    회원 가입

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,12 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import hello.hellospring.service.MemberService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -14,4 +18,18 @@ public class MemberController {
 
         this.memberService = memberService;
     }
+
+     @GetMapping("/member/new")
+    public String createForm(){
+        return "members/createMembersForm";
+     }
+
+     @PostMapping("/members/new")
+    public String crate(@NotNull MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/";
+     }
 }
